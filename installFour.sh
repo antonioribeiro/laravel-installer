5c542ea25d9fb6a6c9138f5dc9aeed984487d136
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2013-01-27 20:30:27 -0200

    updating version
    Signed-off-by: Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>

--- installFour.sh
+++ installFour.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-L4I_VERSION=1.6.0
-L4I_BRANCH=master
+L4I_VERSION=1.6.5
+L4I_BRANCH=v1.6.5
 LARAVEL_APP_DEFAULT_REPOSITORY="https://github.com/laravel/laravel.git"
 LARAVEL_APP_DEFAULT_BRANCH="develop"
 INSTALL_DIR=$1
@@ -103,7 +103,7 @@ function main() {
 function createSite() {
 	loadPackagesArray
 	exit 1
-	
+
 	showHeader
 	cleanL4IRepository
 	createLogDirectory
